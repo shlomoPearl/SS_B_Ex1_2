ccab1130f7514126d4c0c2d7ce9b7222682b709f
shlomoPearl <shlomoper94@gmail.com>
2022-03-15 23:27:04 +0200

    input added

--- Main.cpp
+++ Main.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include"mat.hpp"
+#include"mat.cpp"
 #include<string>
 #include<vector>
 
@@ -7,11 +8,23 @@ int main(){
     using namespace ariel;
     using namespace std;
     int row = 7;
-    int column = 7;
-    string a = " ";
-    a = mat(column,row,'*','-');
+    int column = 9;
+    char c1 = '#';
+    char c2 = '-';
+    string a;
+    a = mat(column,row,c1,c2);
     for(int i = 0 ; i< a.size(); i++){
         cout << a.at(i);
     }
+    cout << endl<< "Enter mat size and design:"<< endl;
+    cin >> column;
+    cin >> row;
+    cin >> c1;
+    cin >> c2;
+    a = mat(column,row,c1,c2);
+    for(int i = 0 ; i< a.size(); i++){
+        cout << a.at(i);
+    }
+
     return 0;
 }
